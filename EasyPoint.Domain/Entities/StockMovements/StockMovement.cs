28e6dcd54979aee2f02673c92ff690e1e43ca997
antony-souza <antonygustavo09@hotmail.com>
2026-08-10 22:18:29 -0300

    feat: Add StockMovement and Stock entity classes to manage stock movements and product stocks

---
+++ EasyPoint.Domain/Entities/StockMovements/StockMovement.cs
@@ -0,0 +1,11 @@
+﻿using EasyPoint.Domain.Common.Entities;
+
+namespace EasyPoint.Domain.Entities.StockMovement;
+
+public class StockMovement : Entity
+{
+    public Guid StockId { get; set; }
+    public Stock Stock { get; set; } = null!;
+    public bool IsExit { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ EasyPoint.Domain/Entities/Stocks/Stock.cs
@@ -0,0 +1,14 @@
+﻿using EasyPoint.Domain.Common.Entities;
+using EasyPoint.Domain.Entities.Products;
+using EasyPoint.Domain.Entities.Stores;
+
+namespace EasyPoint.Domain.Entities.Stock;
+
+public class Stock : Entity
+{
+    public Guid StoreId { get; set; }
+    public Store Store { get; set; } = null!;
+
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+}
